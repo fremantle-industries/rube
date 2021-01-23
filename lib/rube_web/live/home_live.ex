a8f66c26100699c84a3ab331fe488b61f278287b
Alex <alex+git@fremantle.io>
2021-01-23 14:06:56 -0800

    Add stateful blockchain statistics

--- lib/rube_web/live/home_live.ex
+++ lib/rube_web/live/home_live.ex
@@ -4,17 +4,15 @@ defmodule RubeWeb.HomeLive do
   @impl true
   def mount(_params, _session, socket) do
     Phoenix.PubSub.subscribe(Rube.PubSub, "events:event_received")
-    Phoenix.PubSub.subscribe(Rube.PubSub, "heads:new_head_received")
     Phoenix.PubSub.subscribe(Rube.PubSub, "recent_heads:new_head_received")
+    Phoenix.PubSub.subscribe(Rube.PubSub, "blockchain_statistics:new_stats")
+    stats = Rube.BlockchainStatistics.get()
 
     socket =
       socket
-      |> assign(initial_block_number: nil)
-      |> assign(last_block_number: nil)
-      |> assign(last_block_received_at: nil)
-      |> assign(total_events: 0)
-      |> assign(last_event_received_at: nil)
-      |> assign(mounted_at: System.monotonic_time())
+      |> assign(last_head_received_at: stats.last_head_received_at)
+      |> assign(block_cadence_seconds: stats.block_cadence_seconds)
+      |> assign(events_per_second: stats.events_per_second)
       |> assign(recent_blocks: Rube.RecentHeads.queue())
       |> assign(recent_events: Rube.RecentEvents.queue())
 
@@ -32,19 +30,7 @@ defmodule RubeWeb.HomeLive do
 
     socket =
       socket
-      |> assign(total_events: socket.assigns.total_events + 1)
       |> assign(recent_events: recent_events)
-
-    {:noreply, socket}
-  end
-
-  @impl true
-  def handle_info({"heads:new_head_received", _blockchain_id, block_number}, socket) do
-    socket =
-      socket
-      |> assign(initial_block_number: socket.assigns.initial_block_number || block_number)
-      |> assign(last_block_number: block_number)
-      |> assign(last_block_received_at: System.monotonic_time())
 
     {:noreply, socket}
   end
@@ -58,35 +44,28 @@ defmodule RubeWeb.HomeLive do
     {:noreply, socket}
   end
 
-  defp last_block_received_at(nil), do: nil
+  @impl true
+  def handle_info(
+        {"blockchain_statistics:new_stats", stats},
+        socket
+      ) do
+    socket =
+      socket
+      |> assign(last_head_received_at: stats.last_head_received_at)
+      |> assign(block_cadence_seconds: stats.block_cadence_seconds)
+      |> assign(events_per_second: stats.events_per_second)
 
-  defp last_block_received_at(received_at) when is_integer(received_at) do
+    {:noreply, socket}
+  end
+
+  defp last_head_received_at(nil), do: nil
+
+  defp last_head_received_at(received_at) when is_integer(received_at) do
     received_latency =
       System.convert_time_unit(System.monotonic_time() - received_at, :native, :millisecond)
 
     unix_now = "Etc/UTC" |> DateTime.now!() |> DateTime.to_unix(:millisecond)
     unix_now - received_latency
-  end
-
-  defp block_cadence_seconds(nil, nil, _), do: 0
-
-  defp block_cadence_seconds(last, initial, mounted_at) when last == initial,
-    do: block_cadence_seconds(last, last - 1, mounted_at)
-
-  defp block_cadence_seconds(last, initial, mounted_at) do
-    now = System.monotonic_time()
-    seconds_passed = System.convert_time_unit(now - mounted_at, :native, :millisecond) / 1000
-    total_blocks = last - initial
-
-    Float.round(seconds_passed / total_blocks, 2)
-  end
-
-  defp events_per_second(0, _), do: 0
-
-  defp events_per_second(total_events, mounted_at) do
-    now = System.monotonic_time()
-    seconds_passed = System.convert_time_unit(now - mounted_at, :native, :millisecond) / 1000
-    Float.round(seconds_passed / total_events, 2)
   end
 
   defp format_event_attr(value) when is_binary(value) and byte_size(value) == 20 do
